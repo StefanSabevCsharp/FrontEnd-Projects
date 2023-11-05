c1a0765f7d2e22a7ec8ff8a79e61d841b397261e
Stefan Sabev <stevenbg91@gmail.com>
2023-11-05 18:53:02 +0100

    Creating Test WebPage

---
+++ TestWebpage/smooth-scroll.js
@@ -0,0 +1,19 @@
+export function smoothScroll() {
+    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+    
+    for (const link of smoothScrollLinks) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                const yOffset = targetElement.getBoundingClientRect().top + window.pageYOffset;
+                window.scrollTo({
+                    top: yOffset,
+                    behavior: "smooth"
+                });
+            }
+        });
+    }
+}
