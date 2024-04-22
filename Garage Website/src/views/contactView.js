7c5db2c89b65208e8dd03d10a4a746c0b680bb29
Stefan Sabev <stevenbg91@gmail.com>
2024-04-22 20:04:51 +0200

    contactView

--- Garage Website/src/views/contactView.js
+++ Garage Website/src/views/contactView.js
@@ -21,8 +21,19 @@ let contactTemplate = () => html`
                 <p>support@lcgarage.com</p>
             </div>
         </div>
-        <div class="map">
+      
     </div>
+    <div class="map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2732.0010059499346!2d6.6234554762629685!3d46.78458454437944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478dcf7c7b090969%3A0x8e75231784188d37!2sRue%20des%20Uttins%2030b%2C%201400%20Yverdon-les-Bains!5e0!3m2!1sen!2sch!4v1713552942604!5m2!1sen!2sch"
+                width="400"
+                height="300"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+        </div>
 </section>`;
 
 export function showContactView() {
